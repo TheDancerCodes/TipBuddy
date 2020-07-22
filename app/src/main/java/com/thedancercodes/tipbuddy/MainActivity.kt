8fde3f55496f018d789eaa37ac5267a7daef347b
Roger Taracha <rojtaracha@gmail.com>
2020-07-22 19:24:03 +0300

    BaseEditText Logic.

--- app/src/main/java/com/thedancercodes/tipbuddy/MainActivity.kt
+++ app/src/main/java/com/thedancercodes/tipbuddy/MainActivity.kt
@@ -2,6 +2,8 @@ package com.thedancercodes.tipbuddy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
@@ -36,6 +38,24 @@ class MainActivity : AppCompatActivity() {
             override fun onStartTrackingTouch(p0: SeekBar?) {}
 
             override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+        })
+
+        // 6. Add a listener for when we update the base value edit text
+        baseEditText.addTextChangedListener(object: TextWatcher {
+
+            // Called after the user has changed some text
+            override fun afterTextChanged(editable: Editable?) {
+
+                // Log statement to verify the functionality of this method.
+                // Log the method name and the value (What the user has been typing).
+                Log.i(TAG, "afterTextChanged $editable")
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
         })
     }
