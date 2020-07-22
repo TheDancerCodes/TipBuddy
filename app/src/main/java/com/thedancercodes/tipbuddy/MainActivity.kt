620ae60ecedd18d989a29805b1f1fdeb6246254e
Roger Taracha <rojtaracha@gmail.com>
2020-07-22 21:37:47 +0300

    Tip And Total Compute Logic

--- app/src/main/java/com/thedancercodes/tipbuddy/MainActivity.kt
+++ app/src/main/java/com/thedancercodes/tipbuddy/MainActivity.kt
@@ -11,6 +11,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 // Constants
 private const val TAG = "MainActivity"
 private const val INITIAL_TIP_PERCENT = 15
+private const val INITIAL_EMPTY_VALUE = 0.00
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +21,9 @@ class MainActivity : AppCompatActivity() {
         // Set initial SeekBar and TextView Tip value when you initialize the app.
         tipSeekBar.progress = INITIAL_TIP_PERCENT
         tvTipPercent.text = "$INITIAL_TIP_PERCENT%"
+
+        tvTipAmount.text = "$INITIAL_EMPTY_VALUE"
+        tvTotalAmount.text = "$INITIAL_EMPTY_VALUE"
 
         // 1. Notifies you that the user has changed the SeekBar
         tipSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
@@ -33,6 +37,9 @@ class MainActivity : AppCompatActivity() {
                 // Convert the progress value to a string and concatenate a % symbol after the value
                 tvTipPercent.text = "$progress%"
 
+                // 9. Call method for SeekBar functionality to work
+                        computeTipAndTotal()
+
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {}
@@ -42,7 +49,7 @@ class MainActivity : AppCompatActivity() {
         })
 
         // 6. Add a listener for when we update the base value edit text
-        baseEditText.addTextChangedListener(object: TextWatcher {
+        baseEditText.addTextChangedListener(object : TextWatcher {
 
             // Called after the user has changed some text
             override fun afterTextChanged(editable: Editable?) {
@@ -51,6 +58,8 @@ class MainActivity : AppCompatActivity() {
                 // Log the method name and the value (What the user has been typing).
                 Log.i(TAG, "afterTextChanged $editable")
 
+                computeTipAndTotal()
+
             }
 
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
@@ -58,5 +67,41 @@ class MainActivity : AppCompatActivity() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
         })
+    }
+
+
+    // 7. Everytime the base amount changes, we want to calculate the tip and total values.
+    fun computeTipAndTotal() {
+
+        // 10 - Conditional check to fix java.lang.NumberFormatException: empty String crash,
+        // when edit text is empty.
+        if (baseEditText.text.isEmpty()) {
+
+            // 10b) Update values of tip and total to empty & return here
+            tvTipAmount.text = "$INITIAL_EMPTY_VALUE"
+            tvTotalAmount.text = "$INITIAL_EMPTY_VALUE"
+            return // This ensures the logic in 7b isn't run. 👍
+        }
+
+        //7b) Get the value of the base and tip percent
+        // Once we have these values, we can calculate the amount for the tip, (tip% x base)
+        // We can also calculate the value for the total (tip + base)
+        // Base value - value inside edit text
+        // Call to string to change int to string. toDouble so that we can do math.
+        val baseAmount = baseEditText.text.toString().toDouble()
+
+        // Tip %age - The progress of the SeekBar
+        val tipPercent = tipSeekBar.progress
+
+        // Calculate the tip amount
+        val tipAmount = baseAmount * tipPercent / 100
+
+        // Calculate total amount
+        val totalAmount = baseAmount + tipAmount
+
+        // 8) NOTE: After the calculation, place the tipAmount and totalAmount, into
+        // the respective textViews.
+        tvTipAmount.text = "%.2f".format(tipAmount)
+        tvTotalAmount.text = "%.2f".format(totalAmount)
     }
 }
